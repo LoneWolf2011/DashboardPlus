c72639cba59e72c1d4be5f0cf5d01fac8b368f14
Roelof <r_j_vangolen@hotmail.com>
2018-03-08 12:12:05 +0100

    Edit

--- view/layout/sidenav.layout.php
+++ view/layout/sidenav.layout.php
@@ -16,6 +16,7 @@
                 </li>
                 <li><a href="<?= URL_ROOT.'view/home/';?>"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span></a></li>
                 <li><a href="<?= URL_ROOT.'view/ticket/';?>"><i class="fa fa-ticket"></i> <span class="nav-label">Tickets</span></a></li>
+                <li><a href="<?= URL_ROOT.'view/tools/';?>"><i class="fa fa-wrench"></i> <span class="nav-label">Tools</span></a></li>
 				
 				<?php if(htmlentities($_SESSION['user']['user_role'], ENT_QUOTES, 'UTF-8') == 1){ ?>
                 <li><a href="<?= URL_ROOT.'view/settings/';?>"><i class="fa fa-gear"></i> <span class="nav-label">Settings</span></a></li>
