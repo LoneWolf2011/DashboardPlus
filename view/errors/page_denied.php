85d5c71755f0c0e94e95c7f6d77c25a7773eaa48
Roelof <r_j_vangolen@hotmail.com>
2018-03-22 15:20:38 +0100

    Edit error pages & nl lang file

--- view/errors/page_denied.php
+++ view/errors/page_denied.php
@@ -1,13 +1,13 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= APP_LANG;?>">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel='shortcut icon' type='image/x-icon' href='<?= URL_ROOT_IMG; ?>/leaf.ico' />
+	<link rel='shortcut icon' type='image/x-icon' href='<?= URL_ROOT_IMG; ?>/<?= FAVICON_NAME; ?>' />
 	
     <title><?= APP_TITLE;?> | Denied</title>
 
@@ -20,17 +20,17 @@
 
   </head>
 
-<body class="gray-bg">
+<body class="dark-bg"  id="i18container">
 
     <div class="middle-box text-center animated fadeInDown">
         <h1 class="text-danger">X</h1>
-        <h3 class="font-bold">Toegang geweigerd</h3>
+        <h3 class="font-bold" data-i18n="[html]error_page.denied.label">Toegang geweigerd</h3>
 
         <div class="error-desc">
-		   <p>Denk je dat je hier iets te zoeken hebt? Vraag dan na bij een admin waarom je geen toegang hebt.
+			<p data-i18n="[html]error_page.denied.msg">Denk je dat je hier iets te zoeken hebt? Vraag dan na bij een admin waarom je geen toegang hebt.
 			</p>
 			
-			<a href="<?= URL_ROOT; ?>/Src/login/redirect.php" class='btn btn-primary' >Return to safety</a>
+			<a href="<?= URL_ROOT; ?>" class='btn btn-primary' data-i18n="[html]error_page.return_btn" >Return to safety</a>
         </div>
     </div>
 
