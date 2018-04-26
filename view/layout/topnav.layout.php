47d88db6517107fd071e5b7b6ae6a69b41ea181f
Roelof <r_j_vangolen@hotmail.com>
2018-04-26 11:14:33 +0200

    Added sidbar tooltips translations

--- view/layout/topnav.layout.php
+++ view/layout/topnav.layout.php
@@ -1,25 +1,25 @@
-        <div class="row border-bottom">
-            <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <a class="navbar-minimalize minimalize-styl-2 hidden" href="#" id="menu_bar"><i class="stroke-hamburgermenu"></i> </a>
-					
-                    <div class="navbar-form-custom" >
-                        <div class="form-group">
-                            <input type="text" class="form-control autocomplete-append site-nr"  id="top_search" data-i18n="[placeholder]layout.topnav.placeholder" ></input>
-                        </div>
-                    </div>
+<div class="row border-bottom">
+    <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 hidden" href="#" id="menu_bar"><i class="stroke-hamburgermenu"></i> </a>
+			
+            <div class="navbar-form-custom" >
+                <div class="form-group">
+                    <input type="text" class="form-control autocomplete-append site-nr"  id="top_search" data-i18n="[placeholder]layout.topnav.placeholder" ></input>
                 </div>
-                <ul class="nav navbar-top-links navbar-right">
-					<li>
-						<a style="pointer:none;"><i class="fa fa-user"></i> <span id="user_span"><?= htmlspecialchars($_SESSION[SES_NAME]['user_email'], ENT_QUOTES, 'UTF-8');?></span></a>
-					</li>
-                    <li>
-                        <a href="<?= URL_ROOT;?>/Src/controllers/login.controller.php?logout&csrf=<?= $_SESSION['db_token'];?>">
-                            <i class="fa fa-sign-out"></i> <span data-i18n="[html]layout.topnav.logout" id="log_out_span"> Log out </span>
-                        </a>
-                    </li>
-
-                </ul>
-
-            </nav>
+            </div>
         </div>
+        <ul class="nav navbar-top-links navbar-right">
+			<li>
+				<a style="pointer:none;"><i class="fa fa-user"></i> <span id="user_span"><?= htmlspecialchars($_SESSION[SES_NAME]['user_email'], ENT_QUOTES, 'UTF-8');?></span></a>
+			</li>
+            <li>
+                <a href="<?= URL_ROOT;?>/Src/controllers/login.controller.php?logout&csrf=<?= $_SESSION['db_token'];?>">
+                    <i class="fa fa-sign-out"></i> <span data-i18n="[html]layout.topnav.logout" id="log_out_span"> Log out </span>
+                </a>
+            </li>
+
+        </ul>
+
+    </nav>
+</div>
